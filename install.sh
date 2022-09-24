f6e262bf2bac8de384b77be60f411050b0076ab8
dogemackenzie <112769895+dogemackenzie@users.noreply.github.com>
2022-09-23 17:46:43 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,38 +8,26 @@ echo "Installing... Please wait, have pancake and a smoke!"
 
 sleep 3
 
-sudo rm -rf /usr/lib/node_modules
-sudo rm -rf node_modules
-sudo apt remove --purge -y nodejs node
-sudo rm /etc/apt/sources.list.d/*
-sudo apt update
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt install -y apt-transport-https software-properties-common build-essential autoconf pkg-config make gcc g++ screen wget curl ntp fail2ban 
-
+sudo apt install -y apt-transport-https software-properties-common build-essential autoconf pkg-config make gcc g++ screen wget curl ntp fail2ban
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
-
 sudo apt update
 sudo apt install -y libdb4.8-dev libdb4.8++-dev libssl-dev libboost-all-dev libminiupnpc-dev libtool autotools-dev redis-server
-sudo apt install -y sudo git npm nodejs      
-
 sudo systemctl enable fail2ban
 sudo systemctl start fail2ban
 sudo systemctl enable redis-server
 sudo systemctl start redis-server
 sudo systemctl enable ntp
 sudo systemctl start ntp
-
 sudo rm -rf ~/.nvm
 sudo rm -rf ~/.npm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
 sudo chown -R $USER:$GROUP ~/.nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install v12.13.0
-nvm use v12.13.0
+nvm install v12.22.12
+nvm use v12.22.12
 npm update -g
 
 npm install -g webpack@4.46.0
