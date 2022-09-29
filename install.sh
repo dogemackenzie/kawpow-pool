e20875c673e754d8acc477ee32ecb403d672bd2f
dogemackenzie <112769895+dogemackenzie@users.noreply.github.com>
2022-09-28 19:41:27 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,46 +2,34 @@
 
 # This is the Pool install script.
 echo "Pool install script."
-echo "Do NOT run as "root", run as the "pool" user!"
 
 echo "$(tput setaf 1)$(tput setab 7)Installing... Please wait, have a pancake and a smoke! Its going to take a while!$(tput sgr 0)"
 
 sleep 3
 
-sudo rm -rf /usr/lib/node_modules
-sudo rm -rf node_modules
-sudo apt remove --purge -y nodejs node
-sudo rm /etc/apt/sources.list.d/*
-sudo apt update
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt install -y apt-transport-https software-properties-common build-essential autoconf pkg-config make gcc g++ screen wget curl ntp fail2ban 
+rm -rf /usr/lib/node_modules
+rm -rf node_modules
+apt remove --purge -y nodejs node
+rm /etc/apt/sources.list.d/*
 
-sudo add-apt-repository -y ppa:bitcoin/bitcoin
 
-sudo apt update
-sudo apt install -y libdb4.8-dev libdb4.8++-dev libssl-dev libboost-all-dev libminiupnpc-dev libtool autotools-dev redis-server
-sudo apt install -y sudo git npm nodejs      
 
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
-sudo systemctl enable redis-server
-sudo systemctl start redis-server
-sudo systemctl enable ntp
-sudo systemctl start ntp
+service fail2ban start
+service fail2ban enable
+service redis-server start
+service redis-server enable
+service ntp start
+service ntp enable
 
 echo "$(tput setaf 1)$(tput setab 7)Still Installing...5 more minutes Turkish!$(tput sgr 0)"
 
-
-sleep 3
-
-sudo rm -rf ~/.nvm
-sudo rm -rf ~/.npm
+rm -rf ~/.nvm
+rm -rf ~/.npm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
-sudo chown -R $USER:$GROUP ~/.nvm
+chown -R $USER:$GROUP ~/.nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install v12.13.0
 nvm use v12.13.0
@@ -56,13 +44,6 @@ npm audit fix
 npm install sha3
 npm install logger
 
-#npm rebuild
-
-sleep 5
-
-#npm i
-
 echo "$(tput setaf 0)$(tput setab 7)Installation completed Ya Bastard!! Must construct additional Pylons!$(tput sgr 0)"
-
 
 exit 0
